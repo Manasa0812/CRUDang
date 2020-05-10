7365f4a9bb5dd3d8313ef5e1ef6fb26416bb7ee4
Manasa <batchusaimanasa08@gmail.com>
2020-05-11 00:25:17 +0530

    login and signup details to database
    forgot password .....has to be done using node mailer by sending otp to mails

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var monk=require('monk');
+var ran=require('randomstring');
+var nodemailer=require('nodemailer')
+
 var db=monk('localhost:27017/codeheat');
 var col=db.get('ang');
-
+var col1=db.get('signup');
+// var col2=db.get('login');
 /* GET home page. */
 
 router.get('/index', function(req, res, next) {
@@ -48,11 +52,56 @@ router.delete('/deldata/:id',function(req,res){
 
 router.get('/',function(req,res){
 	res.render('login_signup');
+});
+router.post('/postsignup',function(req,res){
+	console.log(req.body);
+	col1.insert(req.body,function(err,docs){
+		if(err){
+			console.log(err)
+		}else{
+			// console.log("success");
+			// console.log(docs);
+			res.send(docs);
+		}
+	});
+});
+
+router.post('/postlogin',function(req,res){
+// console.log(req.body.email);
+// console.log(req.body.pass);
+var em1=req.body.email;
+var p1=req.body.pass;
+col1.findOne({"email":em1,"pass":p1},function(err,docs){
+	if(docs){
+		console.log(docs);
+		res.send(docs)
+	}else{
+		console.log("login not successful");
+		res.sendStatus(500)
+	}
+})
 })
 
+router.get('/sendlog',function(req,res){
+	res.redirect('/index')
+});
 
+router.get('/comeout',function(req,res){
+	res.redirect('/');
+})
 //-------------------------------------------forgot page--------------------------------
 router.get('/forgot',function(req,res){
 	res.render('forgot')
+});
+router.get('/loginpage',function(req,res){
+	res.render('login_signup');
+});
+
+router.post('/postforgot',function(req,res){
+	console.log(req.body.mail)
+	var mail1=req.body.mail;
+	var otp=ran.generate(5);
+	console.log(otp);
+	col1.update({"email":mail1},{$set:{"pass":otp}})
 })
 module.exports = router;
